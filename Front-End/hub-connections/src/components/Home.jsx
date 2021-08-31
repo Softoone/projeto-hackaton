b6bb1a2d9499133b40b0c4df984a8e587569edb1
unknown <softdev.gss@gmail.com>
2021-08-30 23:03:35 -0300

    feat(Add bootstrap + reactstrap, defining page style)

--- Front-End/hub-connections/src/components/Home.jsx
+++ Front-End/hub-connections/src/components/Home.jsx
@@ -1,26 +1,78 @@
 import "../styles/Home.css";
+import peopleImage from "../assets/download.jpeg";
+import {
+  Row,
+  Card,
+  CardBody,
+  CardText,
+  Button,
+  CardTitle,
+  CardSubtitle,
+  Container,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Navbar,
+  NavbarBrand,
+} from "reactstrap";
 
 const Home = () => {
   return (
-    <div>
-      <nav></nav>
-      <section>
-        <div id="title-text">
-          <h1>Hub Connections</h1>
+    <Container>
+      <Row>
+        <Navbar>
+          <NavbarBrand>Hub Connections</NavbarBrand>
+        </Navbar>
+        <Col>
+          <Card>
+            <CardBody>
+              <CardTitle tag="h3">
+                Get Started in our brand new community
+              </CardTitle>
+              <div id="login-box">
+                <Form>
+                  <FormGroup>
+                    <Label for="exampleEmail">Email</Label>
+                    <Input
+                      type="email"
+                      name="email"
+                      id="exampleEmail"
+                      placeholder="with a placeholder"
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="examplePassword">Password</Label>
+                    <Input
+                      type="password"
+                      name="password"
+                      id="examplePassword"
+                      placeholder="password placeholder"
+                    />
+                  </FormGroup>
+                </Form>
+                <Button color="primary" size="lg">
+                  Sign in
+                </Button>
+                <Button outline color="primary" size="lg">
+                  Sign in with Google
+                </Button>
+              </div>
+            </CardBody>
+          </Card>
+        </Col>
+
+        <Col>
+          <img alt="Imagem a ser adicionada" />
+        </Col>
+
+        <div>
+          <p>Texto descritivo sobre o objetivo/funcionalidades da página</p>
         </div>
-        <div id="login-box">
-          <form></form>
-          <button>Sign in</button>
-          <button>Sign in with Google</button>
-        </div>
-        <div id="title-image">
-          <img></img>
-        </div>
-      </section>
-      <section>
-        <div>Texto descritivo sobre o objetivo/funcionalidades da página</div>
-      </section>
-    </div>
+      </Row>
+      <footer></footer>
+    </Container>
   );
 };
 
