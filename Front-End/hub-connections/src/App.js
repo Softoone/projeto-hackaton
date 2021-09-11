ddd9fa567f75e49599a098604096fa5178da979c
Softoone <softdev.gss@gmail.com>
2021-09-11 12:23:56 -0300

    Mods/Elements_HTML

--- Front-End/hub-connections/src/App.js
+++ Front-End/hub-connections/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Perfil from "./components/Perfil"
 
 function App() {
-  return <Login></Login>;
+  return <Perfil></Perfil>;
 }
 
 export default App;
