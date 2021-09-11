0b44dcec27b406888079e48a7d7610f3fd50e55f
unknown <softdev.gss@gmail.com>
2021-09-11 00:05:02 -0300

    Stylish/Feed&Header:Buttons_Icons_List

--- Front-End/hub-connections/src/components/Feed.jsx
+++ Front-End/hub-connections/src/components/Feed.jsx
@@ -5,7 +5,7 @@ import { Button, Container, Form, Row, Col } from "reactstrap";
 
 const Feed = () => {
   return (
-    <div>
+    <div className="postsFeed">
       <Header></Header>
 
       {/* Create Post */}
@@ -39,98 +39,129 @@ const Feed = () => {
       {/* List of Posts */}
 
       <section className="secondLine">
-        <Row>
-          <Col md="12" id="mainFeed">
-            <ul className="posts" id="posts">
-              <li className="post">
-                <Col md="12" id="userInfoPost">
-                  <img src="pessoa.jpg" alt="Profile Icon" />
-                  <strong>User Name</strong>
-                  <span>Datetime</span>
-                  <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Ratione perspiciatis accusantium nesciunt aut, corrupti amet
-                    cupiditate velit. Incidunt pariatur deserunt amet, magni
-                    voluptatibus inventore possimus. Voluptatem architecto
-                    quibusdam reiciendis modi.
-                  </p>
-                </Col>
-                <div id="postActions">
-                  <Button className="actionButton">
-                    <img src="curtir.png" /*alt="Like/Fav"*/ />
-                    Like/Fav
-                  </Button>
-                  <Button className="actionButton">
-                    <img src="compartilhar.png" /* alt="Share" */ />
-                    Share
-                  </Button>
-                  <Button className="actionButton">
-                    <img src="comentar.png" /* alt="Comment" */ />
-                    Comment
-                  </Button>
-                </div>
-              </li>
+        <Container fluid={true}>
+          <Row>
+            <Col md="12" id="mainFeed">
+              <ul className="posts" id="posts">
+                <li className="post">
+                  <Col md="12" id="userInfoPost">
+                    <img src="pessoa.jpg" alt="Profile Icon" />
+                    <strong>User Name</strong>
+                    <span>Datetime</span>
+                    <p>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Ratione perspiciatis accusantium nesciunt aut, corrupti
+                      amet cupiditate velit. Incidunt pariatur deserunt amet,
+                      magni voluptatibus inventore possimus. Voluptatem
+                      architecto quibusdam reiciendis modi.
+                    </p>
+                  </Col>
+                  <div id="postActions">
+                    <Button className="actionButton">
+                      <img src="curtir.png" /*alt="Like/Fav"*/ />
+                      Like/Fav
+                    </Button>
+                    <Button className="actionButton">
+                      <img src="compartilhar.png" /* alt="Share" */ />
+                      Share
+                    </Button>
+                    <Button className="actionButton">
+                      <img src="comentar.png" /* alt="Comment" */ />
+                      Comment
+                    </Button>
+                  </div>
+                </li>
 
-              <li className="post">
-                <Col md="12" id="userInfoPost">
-                  <img src="pessoa.jpg" alt="Profile Icon" />
-                  <strong>User Name</strong>
-                  <span>Datetime</span>
-                  <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Ratione perspiciatis accusantium nesciunt aut, corrupti amet
-                    cupiditate velit. Incidunt pariatur deserunt amet, magni
-                    voluptatibus inventore possimus. Voluptatem architecto
-                    quibusdam reiciendis modi.
-                  </p>
-                </Col>
-                <div id="postActions">
-                  <Button className="actionButton">
-                    <img src="curtir.png" /*alt="Like/Fav"*/ />
-                    Like/Fav
-                  </Button>
-                  <Button className="actionButton">
-                    <img src="compartilhar.png" /* alt="Share" */ />
-                    Share
-                  </Button>
-                  <Button className="actionButton">
-                    <img src="comentar.png" /* alt="Comment" */ />
-                    Comment
-                  </Button>
-                </div>
-              </li>
+                <li className="post">
+                  <Col md="12" id="userInfoPost">
+                    <img src="pessoa.jpg" alt="Profile Icon" />
+                    <strong>User Name</strong>
+                    <span>Datetime</span>
+                    <p>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Ratione perspiciatis accusantium nesciunt aut, corrupti
+                      amet cupiditate velit. Incidunt pariatur deserunt amet,
+                      magni voluptatibus inventore possimus. Voluptatem
+                      architecto quibusdam reiciendis modi.
+                    </p>
+                  </Col>
+                  <div id="postActions">
+                    <Button className="actionButton">
+                      <img src="curtir.png" /*alt="Like/Fav"*/ />
+                      Like/Fav
+                    </Button>
+                    <Button className="actionButton">
+                      <img src="compartilhar.png" /* alt="Share" */ />
+                      Share
+                    </Button>
+                    <Button className="actionButton">
+                      <img src="comentar.png" /* alt="Comment" */ />
+                      Comment
+                    </Button>
+                  </div>
+                </li>
 
-              <li className="post">
-                <Col md="12" id="userInfoPost">
-                  <img src="pessoa.jpg" alt="Profile Icon" />
-                  <strong>User Name</strong>
-                  <span>Datetime</span>
-                  <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Ratione perspiciatis accusantium nesciunt aut, corrupti amet
-                    cupiditate velit. Incidunt pariatur deserunt amet, magni
-                    voluptatibus inventore possimus. Voluptatem architecto
-                    quibusdam reiciendis modi.
-                  </p>
-                </Col>
-                <div id="postActions">
-                  <Button className="actionButton">
-                    <img src="curtir.png" /*alt="Like/Fav"*/ />
-                    Like/Fav
-                  </Button>
-                  <Button className="actionButton">
-                    <img src="compartilhar.png" /* alt="Share" */ />
-                    Share
-                  </Button>
-                  <Button className="actionButton">
-                    <img src="comentar.png" /* alt="Comment" */ />
-                    Comment
-                  </Button>
-                </div>
-              </li>
-            </ul>
-          </Col>
-        </Row>
+                <li className="post">
+                  <Col md="12" id="userInfoPost">
+                    <img src="pessoa.jpg" alt="Profile Icon" />
+                    <strong>User Name</strong>
+                    <span>Datetime</span>
+                    <p>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Ratione perspiciatis accusantium nesciunt aut, corrupti
+                      amet cupiditate velit. Incidunt pariatur deserunt amet,
+                      magni voluptatibus inventore possimus. Voluptatem
+                      architecto quibusdam reiciendis modi.
+                    </p>
+                  </Col>
+                  <div id="postActions">
+                    <Button className="actionButton">
+                      <img src="curtir.png" /*alt="Like/Fav"*/ />
+                      Like/Fav
+                    </Button>
+                    <Button className="actionButton">
+                      <img src="compartilhar.png" /* alt="Share" */ />
+                      Share
+                    </Button>
+                    <Button className="actionButton">
+                      <img src="comentar.png" /* alt="Comment" */ />
+                      Comment
+                    </Button>
+                  </div>
+                </li>
+
+                <li className="post">
+                  <Col md="12" id="userInfoPost">
+                    <img src="pessoa.jpg" alt="Profile Icon" />
+                    <strong>User Name</strong>
+                    <span>Datetime</span>
+                    <p>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Ratione perspiciatis accusantium nesciunt aut, corrupti
+                      amet cupiditate velit. Incidunt pariatur deserunt amet,
+                      magni voluptatibus inventore possimus. Voluptatem
+                      architecto quibusdam reiciendis modi.
+                    </p>
+                  </Col>
+                  <div id="postActions">
+                    <Button className="actionButton">
+                      <img src="curtir.png" /*alt="Like/Fav"*/ />
+                      Like/Fav
+                    </Button>
+                    <Button className="actionButton">
+                      <img src="compartilhar.png" /* alt="Share" */ />
+                      Share
+                    </Button>
+                    <Button className="actionButton">
+                      <img src="comentar.png" /* alt="Comment" */ />
+                      Comment
+                    </Button>
+                  </div>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
       </section>
 
       <Footer></Footer>
