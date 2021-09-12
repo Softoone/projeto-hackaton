bc40ae2ef72d05966d407a485e888f74fbb56e23
unknown <softdev.gss@gmail.com>
2021-09-12 18:23:31 -0300

    Mods/Perfil_CSS

--- Front-End/hub-connections/src/components/Perfil.jsx
+++ Front-End/hub-connections/src/components/Perfil.jsx
@@ -1,41 +1,41 @@
-import { Container, Row, Col } from "reactstrap"
-import "../styles/Perfil.css"
-import Footer from "./Footer"
-import Header from "./Header"
+import { Container, Row, Col } from "reactstrap";
+import "../styles/Perfil.css";
+import Footer from "./Footer";
+import Header from "./Header";
 
 const Perfil = () => {
-    return(
-        <div>
-            <Header></Header>
-            <section className="userInfo">
-                <Container fluid={true}>
-                    <Row>
-                        <div className="userInfoCard">
-                            <Col md="12" id="banner">
-                                Banner
-                            </Col>
-                            <img src="" alt="Icone de Perfil do Usuário" />
-                            <Col md="12" id="description"></Col>
-                        </div>
-                    </Row>
-                </Container>
-            </section>
+  return (
+    <div>
+      <Header></Header>
+      <section className="userInfo">
+        <Container fluid={true}>
+          <Row>
+            <div className="userInfoCard">
+              <Col md="12" id="banner">
+                Banner
+              </Col>
+              <img src="" alt="Icone de Perfil do Usuário" />
+              <Col md="12" id="description"></Col>
+            </div>
+          </Row>
+        </Container>
+      </section>
 
-            <section>
-                <Container>
-                    <div className="portfolio">
-                        <h2>PortFolio</h2>
-                        <div className="fileUpload"></div>
-                        <div className="fileUpload"></div>
-                        <div className="fileUpload"></div>
-                        <div className="fileUpload"></div>
-                    </div>
-                </Container>
-            </section>
+      <section>
+        <Container>
+          <div className="portfolio">
+            <h2>PortFolio</h2>
+            <div className="fileUpload"></div>
+            <div className="fileUpload"></div>
+            <div className="fileUpload"></div>
+            <div className="fileUpload"></div>
+          </div>
+        </Container>
+      </section>
 
-            <Footer></Footer>
-        </div>
-    )
-}
+      <Footer></Footer>
+    </div>
+  );
+};
 
-export default Perfil
+export default Perfil;
